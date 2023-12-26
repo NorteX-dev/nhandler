a47a52678df9360640bec9398b82f300dd130acc
NorteX <contact@nortexdev.com>
2023-12-26 20:49:22 +0100

    add reexports

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,20 @@
 export * from "./classes/CommandHandler";
 export * from "./classes/ComponentHandler";
 export * from "./classes/EventHandler";
+export * from "./classes/ContextMenuHandler";
+
 export * from "./errors/CommandError";
 export * from "./errors/ComponentError";
+export * from "./errors/ContextMenuActionError";
+
 export * from "./functions/createCommands";
 export * from "./functions/createComponents";
 export * from "./functions/createEvents";
+export * from "./functions/createContextMenu";
+
 export * from "./interfaces/Command";
 export * from "./interfaces/Component";
 export * from "./interfaces/Event";
+export * from "./interfaces/ContextMenuAction";
+
 export * from "./util";
