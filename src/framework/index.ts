340fda3d328441331ab553723ebbfb7ac8f2fb13
NorteX <contact@nortexdev.com>
2024-03-10 18:41:11 +0100

    Remove framework

--- src/framework/index.ts
+++
@@ -1,6 +0,0 @@
-// Files seen in this folder `framework` should be imported using `nhandler/framework`.
-
-export * from "./embeds";
-export * from "./modules";
-export * from "./logger";
-export * from "./configParser";
